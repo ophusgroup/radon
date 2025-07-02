140742c3c54df4bcd55bdf865ccdaf4c18e38bd0
darshan-mali <darshanmali130@gmail.com>
2025-07-02 11:13:07 -0700

    Adding header to see if it breaks

--- src/radon/cuda/_wrapper.py
+++ src/radon/cuda/_wrapper.py
@@ -2,7 +2,7 @@ from . import _cuda_add
 
 
 def add_arrays(a, b, c):
-    return _cuda_add.add_arrays(a, b, c)
+    _cuda_add.add_arrays(a, b, c)
 
 def subtract_arrays(a, b, c):
-    return _cuda_add.subtract_arrays(a, b, c)
+    _cuda_add.subtract_arrays(a, b, c)
