dea1ac05fbb271d5d424a6e4b548ed472a36049c
arthurmccray <amccray@stanford.edu>
2025-07-02 12:01:49 -0700

    fixing import issues where torch had to be imported first

--- src/radon/__init__.py
+++ src/radon/__init__.py
@@ -3,19 +3,22 @@ GPU-accelerated radon transforms using CUDA and pybind11
 """
 
 try:
-    from .cuda import add_arrays
-
-    __all__ = ["add_arrays"]
-except ImportError:
-    print("Warning: CUDA extension not available. Make sure to build the extension.")
-    __all__ = []
+    # This seems necessary, as otherwise the cuda kernels cannot be loaded, but it feels like
+    # bad practice because now the entirity of torch is accessible from radon.torch
+    import torch as torch
+except ImportError as err:
+    print("how the heck did you get here? torch not installed")
+    raise ImportError("Torch is required for this package.") from err
 
 try:
-    from .cuda import subtract_arrays
+    from .cuda import add_arrays as add_arrays
+    from .cuda import subtract_arrays as subtract_arrays
 
-    __all__ = ["subtract_arrays"]
+    __all__ = ["add_arrays", "subtract_arrays"]
+    print("CUDA extension loaded successfully.")
+
 except ImportError:
-    print("Warning: Error encountered. Check code properly")
+    print("Warning: CUDA extension not available. Make sure to build the extension.")
     __all__ = []
 
 __version__ = "0.1.0"
--- src/radon/cuda/__init__.py
+++ src/radon/cuda/__init__.py
@@ -1,6 +1,3 @@
 from ._wrapper import add_arrays, subtract_arrays
 
 __all__ = ["add_arrays", "subtract_arrays"]
-
-print("CUDA extension loaded successfully.")
-print(add_arrays)
